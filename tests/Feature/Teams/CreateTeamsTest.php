ce8d03ae13f99b0f73d5594a57daaed7236fd5db
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-01-20 20:12:33 -0300

    validation tests for create team

--- tests/Feature/Teams/CreateTeamsTest.php
+++ tests/Feature/Teams/CreateTeamsTest.php
@@ -26,3 +26,30 @@ it('should be able to create a Team',function (){
         'captain_id' => $this->user->getKey()
     ]);
 });
+
+describe('validation tests', function (){
+
+    test('name::validations', function ($rule, $value){
+        Livewire::test(CreateTeams::class)
+            ->set('name', $value)
+            ->set('captain_id', $this->user->getKey())
+            ->call('save')
+            ->assertHasErrors(['name' => $rule]);
+    })->with([
+        'required' => ['required', ''],
+        'min:3'    => ['min:3', 'aa'],
+        'max:255'    => ['max:255', str_repeat('a',256)],
+    ]);
+
+    test('captain_id::validations', function ($rule, $value){
+        Livewire::test(CreateTeams::class)
+            ->set('name', 'team one')
+            ->set('captain_id', $value)
+            ->call('save')
+            ->assertHasErrors(['captain_id' => $rule]);
+    })->with([
+        'required' => ['required', null],
+        'exists'    => ['exists', 888],
+    ]);
+
+});
