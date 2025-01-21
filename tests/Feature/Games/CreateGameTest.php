84d6cf1034864ace5b6594aaecec946e2080849c
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-01-21 16:03:07 -0300

    Validations for create a team: team1 validations

--- tests/Feature/Games/CreateGameTest.php
+++ tests/Feature/Games/CreateGameTest.php
@@ -9,40 +9,57 @@ use Carbon\Carbon;
 use Livewire\Livewire;
 use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\assertDatabaseHas;
+beforeEach(function (){
+    $this->user = User::factory()->createOne();
+    $this->user2 = User::factory()->createOne();
+    $this->admin = User::factory()->admin()->createOne();
+    $this->championship = Championship::factory()->for($this->admin)->create();
+    $this->team1 = Team::factory()->create([
+        'captain_id' => $this->user->getKey()
+    ]);
+    $this->team2 = Team::factory()->create([
+        'captain_id' => $this->user2->getKey()
+    ]);
+    $this->day = Carbon::tomorrow()->format('Y-m-d');
+});
 
 it('should be able to create a game', function (){
-    $user = User::factory()->createOne();
-    $user2 = User::factory()->createOne();
-    $admin = User::factory()->admin()->createOne();
-    $championship = Championship::factory()->for($admin)->create();
-    $team1 = Team::factory()->create([
-        'captain_id' => $user->getKey()
-    ]);
-    $team2 = Team::factory()->create([
-        'captain_id' => $user2->getKey()
-    ]);
-    $day = Carbon::tomorrow()->format('Y-m-d');
-
-
-    Livewire::actingAs($user)
+    Livewire::actingAs($this->user)
         ->test(CreateGame::class)
-        ->set('team1',$team1->getKey())
-        ->set('team2',$team2->getKey())
-        ->set('day',$day )
+        ->set('team1',$this->team1->getKey())
+        ->set('team2',$this->team2->getKey())
+        ->set('day',$this->day )
         ->call('save')
         ->assertHasNoErrors()
         ->assertOk();
 
     assertDatabaseCount(Game::class,1);
     assertDatabaseHas(Game::class,[
-       'team_1_id' => $team1->getKey(),
-       'team_2_id' => $team2->getKey(),
-       'day'       => $day
+       'team_1_id' => $this->team1->getKey(),
+       'team_2_id' => $this->team2->getKey(),
+       'day'       => $this->day
     ]);
 
     $game = Game::query()->first();
     expect($game)
-        ->team_1_id->toBe($team1->getKey())
-        ->team_2_id->toBe($team2->getKey())
-        ->day->toBe($day);
+        ->team_1_id->toBe($this->team1->getKey())
+        ->team_2_id->toBe($this->team2->getKey())
+        ->day->toBe($this->day);
+});
+
+describe('validation tests', function (){
+
+    test('team1::validations',function ($rule,$value){
+        Livewire::actingAs($this->user)
+            ->test(CreateGame::class)
+            ->set('team1',$value)
+            ->set('team2',$this->team2->getKey())
+            ->set('day',$this->day )
+            ->call('save')
+            ->assertHasErrors(['team1' => $rule]);
+
+    })->with([
+        'required' => ['required', null],
+        'exists'   => ['exists', 9999]
+    ]);
 });
