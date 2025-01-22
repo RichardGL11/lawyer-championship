526d274f26f49fe9c8b254b772867c6e2d0e2fee
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-01-22 14:38:53 -0300

    [test]: validation test for team1

--- tests/Feature/Games/UpdateGameTest.php
+++ tests/Feature/Games/UpdateGameTest.php
@@ -68,3 +68,34 @@ test('normal user can not update an game', function () {
         ->call('update', $game)
         ->assertForbidden();
 });
+
+describe('validation tests', function (){
+
+    test('team1', function($rule,$value){
+        $admin = User::factory()->admin()->create();
+        $team1 = Team::factory()->createOne();
+        $team2 = Team::factory()->createOne();
+        $game = Game::factory()->create([
+            'team_1_id' => $team1->getKey(),
+            'team_2_id' => $team2->getKey(),
+        ]);
+        $day = Carbon::tomorrow()->format('Y-m-d');
+        Livewire::actingAs($admin)
+            ->test(UpdateGame::class)
+            ->set('team1',$value)
+            ->set('team2',$team2->getKey())
+            ->set('goalTeam1', 1)
+            ->set('goalTeam2', 1)
+            ->set('goals', 2)
+            ->set('local', 'street')
+            ->set('day', $day)
+            ->set('winner', $game->team2)
+            ->call('update', $game)
+            ->assertHasErrors(['team1' => $rule]);
+    })->with([
+        'required' => ['required', null],
+        'exists'   => ['exists', 999]
+    ]);
+});
+
+
