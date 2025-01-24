89a7f9ef6394b1cfc8b01152160a6a87d3156ac7
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-01-24 16:55:53 -0300

    making sure only users that dont belongs to the team can solicity to join at the team

--- app/Livewire/Teams/ShowTeamDetails.php
+++ app/Livewire/Teams/ShowTeamDetails.php
@@ -3,16 +3,28 @@
 namespace App\Livewire\Teams;
 
 use App\Models\Team;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class ShowTeamDetails extends Component
 {
     public Team $team;
+    public bool $belongs;
+    public function userBelongsToTheTeam()
+    {
+        if($this->team->users()->where('user_id', Auth::id())->exists()){
+            return $this->belongs = true;
+        };
+
+        return $this->belongs = false;
+    }
 
     public function mount(Team $team):void
     {
         $this->team = $team;
+        $this->userBelongsToTheTeam();
     }
 
     #[Layout('layouts.app')]
--- tests/Feature/Teams/ShowTeamDetailsTest.php
+++ tests/Feature/Teams/ShowTeamDetailsTest.php
@@ -7,10 +7,6 @@ use Livewire\Livewire;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
 
-it('renders successfully', function () {
-    Livewire::test(ShowTeamDetails::class)
-        ->assertStatus(200);
-});
 
 test('loads the information about the team', function () {
     $captain = User::factory()->createOne();
@@ -27,6 +23,7 @@ test('loads the information about the team', function () {
     $response = get(route('teams.show',$team));
     $response->assertSeeText($team->name);
     $response->assertSeeText($team->captain->name);
+    $response->assertSeeText("Ask for participate");
 
     $users->each(function ($user) use ($response){
         $response->assertSee($user->name);
@@ -34,5 +31,30 @@ test('loads the information about the team', function () {
         $response->assertSee($user->goals()->count());
         $response->assertSee($user->assistance()->count());
     });
+
+});
+test('user that belongs to the team cannot see the button for join', function () {
+    $captain = User::factory()->createOne();
+    $users = User::factory(5)->create();
+    $team = Team::factory()->create([
+        'captain_id' => $captain->getKey()
+    ]);
+    $users->each(function ($user) use($team){
+        $team->users()->attach($user);
+    });
+
+    actingAs($captain);
+
+    $response = get(route('teams.show',$team));
+    $response->assertSeeText($team->name);
+    $response->assertSeeText($team->captain->name);
+
+    $users->each(function ($user) use ($response){
+        $response->assertSee($user->name);
+        $response->assertSee($user->email);
+        $response->assertSee($user->goals()->count());
+        $response->assertSee($user->assistance()->count());
+    });
+    $response->assertDontSeeText("Ask for participate");
 
 });
